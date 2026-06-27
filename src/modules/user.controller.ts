3e9af0d33d571f6f6d30a1980f43135484a38b9e
Shaikh Al Nahian <sheikhalnahian@gmail.com>
2026-06-27 09:27:11 +0600

    catchAsync function is created

--- src/modules/user.controller.ts
+++ src/modules/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
 import status from "http-status"
 import { userServices } from "./user.service"
+import { catchAsync } from "../utils/catchAsync"
 
-const registerUser=async(req:Request, res:Response)=>{
+const registerUser=catchAsync(async(req:Request, res:Response)=>{
 
-    try{
         const user = await userServices.registerUserIntoDB(req.body)
 
         res.status(status.CREATED).json({
@@ -15,18 +15,7 @@ const registerUser=async(req:Request, res:Response)=>{
                 user
             }
         })
-    }
-    catch(error:any){
-        console.log(error)
-        res.status(status.INTERNAL_SERVER_ERROR).json({
-            success: false,
-            error: error.message
-        })
-    }
-
-
-
-}
+})
 
 export const userController={
     registerUser
---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import status from "http-status";
+export const catchAsync=(fn:RequestHandler)=>{
+    return async(req:Request, res:Response,next:NextFunction)=>{
+        try{
+            await fn(req,res,next)
+
+        }catch(error:any){
+            console.log(error)
+            res.status(status.INTERNAL_SERVER_ERROR).json({
+                success:false,
+                statusCode: status.INTERNAL_SERVER_ERROR,
+                message:error.message
+            })
+        }
+    }
+}
